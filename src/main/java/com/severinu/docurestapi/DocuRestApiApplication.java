91509a05913fe4b94988875f34cd13709d8c0965
severinum <sewerynmichota@gmail.com>
2021-07-26 02:47:51 +0100

    First cool commit

---
+++ src/main/java/com/severinu/docurestapi/DocuRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.severinu.docurestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocuRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/severinu/docurestapi/DocuRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.severinu.docurestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

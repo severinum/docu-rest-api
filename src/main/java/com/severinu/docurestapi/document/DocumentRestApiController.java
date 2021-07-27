9f245a9e458a828289e96a047f0b36523a590286
severinum <sewerynmichota@gmail.com>
2021-07-27 01:03:29 +0100

    Get document , document title

--- src/main/java/com/severinu/docurestapi/document/DocumentRestApiController.java
+++ src/main/java/com/severinu/docurestapi/document/DocumentRestApiController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/documents")
@@ -12,8 +13,23 @@ public class DocumentRestApiController {
     private Collection<Document> documents = new ArrayList<>();
 
     @GetMapping
-    public Iterable<Document> getDocument() {
+    public Iterable<Document> getDocuments() {
         return this.documents;
+    }
+
+    @GetMapping("/{documentNumber}")
+    public Optional<Document> getDocument(@PathVariable long documentNumber) {
+        return documents.stream()
+                .filter(doc -> doc.getNumber() == documentNumber)
+                .findAny();
+    }
+
+    @GetMapping("/{documentNumber}/title")
+    public Optional<String> getDocumentTitle(@PathVariable long documentNumber) {
+        return documents.stream()
+                .filter( document -> document.getNumber() == documentNumber)
+                .findAny()
+                .map(Document::getTitle);
     }
 
     @PostMapping
